2e181f9e9e984841d660a51ba8f2f6833a00060b
user.email <younsoo6103@naver.com>
2025-06-02 11:47:36 +0900

    step 4, r

--- src/test/java/com/back/AppTest.java
+++ src/test/java/com/back/AppTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AppTest {
+
     @Test
     @DisplayName("`== 명언 앱 ==` 출력")
     void t1() {
@@ -19,5 +20,20 @@ public class AppTest {
 
         assertThat(rs)
                 .contains("== 명언 앱 ==");
+    }
+    @Test
+    @DisplayName("등록")
+    void t2() {
+        String rs = AppTestRunner.run("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                종료
+                """);
+
+        assertThat(rs)
+                .contains("명령) ")
+                .contains("명언 : ")
+                .contains("작가 : ");
     }
 }
