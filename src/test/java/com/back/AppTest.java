8338d1affb0f7bb8f777efbc98b6444e6faafaee
user.email <younsoo6103@naver.com>
2025-06-02 12:08:51 +0900

    step 5, r

--- src/test/java/com/back/AppTest.java
+++ src/test/java/com/back/AppTest.java
@@ -15,6 +15,7 @@ public class AppTest {
         assertThat(rs)
                 .contains("== 명언 앱 ==");
     }
+
     @Test
     @DisplayName("등록")
     void t2() {
@@ -29,5 +30,24 @@ public class AppTest {
                 .contains("명령) ")
                 .contains("명언 : ")
                 .contains("작가 : ");
+    }
+
+    @Test
+    @DisplayName("등록")
+    void t3() {
+        String rs = AppTestRunner.run("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                종료
+                """);
+
+        assertThat(rs)
+                .contains("명령) ")
+                .contains("명언 : ")
+                .contains("작가 : ");
+
+        assertThat(rs)
+                .contains("1번 명언이 등록되었습니다.");
     }
 }
