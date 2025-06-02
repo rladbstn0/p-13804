a74536f29db62183ce1e39473fa2fd14f2ec7554
user.email <younsoo6103@naver.com>
2025-06-02 14:29:45 +0900

    step 8, b3

---
+++ src/main/java/com/back/domain/wiseSaying/controller/WiseSayingController.java
@@ -0,0 +1,37 @@
+package com.back.domain.wiseSaying.controller;
+
+import com.back.domain.wiseSaying.entity.WiseSaying;
+import com.back.domain.wiseSaying.service.WiseSayingService;
+
+import java.util.Scanner;
+
+public class WiseSayingController {
+    private final Scanner scanner;
+    private final WiseSayingService wiseSayingService;
+
+    public WiseSayingController(Scanner scanner) {
+        this.scanner = scanner;
+        this.wiseSayingService = new WiseSayingService();
+    }
+
+    public void actionWrite() {
+        System.out.print("명언 : ");
+        String content = scanner.nextLine();
+
+        System.out.print("작가 : ");
+        String author = scanner.nextLine();
+
+        WiseSaying wiseSaying = wiseSayingService.write(content, author);
+
+        System.out.printf("%d번 명언이 등록되었습니다.\n", wiseSaying.getId());
+    }
+
+    public void actionList() {
+        System.out.println("번호 / 작가 / 명언");
+        System.out.println("----------------------");
+
+        for (WiseSaying wiseSaying : wiseSayingService.findForList()) {
+            System.out.printf("%d / %s / %s\n", wiseSaying.getId(), wiseSaying.getAuthor(), wiseSaying.getContent());
+        }
+    }
+}
