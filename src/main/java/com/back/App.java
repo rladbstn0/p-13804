d44634b4f52e9a4149a2a58747c86ae4a11a48e6
user.email <younsoo6103@naver.com>
2025-06-02 12:26:43 +0900

    step 7, g

--- src/main/java/com/back/App.java
+++ src/main/java/com/back/App.java
@@ -1,5 +1,7 @@
 package com.back;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
@@ -12,6 +14,7 @@ public class App {
     public void run() {
         System.out.println("== 명언 앱 ==");
         int lastId = 0;
+        List<WiseSaying> wiseSayings = new ArrayList<>();
 
 
         while (true) {
@@ -28,7 +31,17 @@ public class App {
 
                     int id = ++lastId;
 
+                    wiseSayings.add(new WiseSaying(id, content, author));
+
                     System.out.printf("%d번 명언이 등록되었습니다.\n", id);
+                }
+                case "목록" -> {
+                    System.out.println("번호 / 작가 / 명언");
+                    System.out.println("----------------------");
+
+                    for(WiseSaying wiseSaying : wiseSayings.reversed()){
+                        System.out.printf("%d / %s / %s\n", wiseSaying.getId(), wiseSaying.getAuthor(), wiseSaying.getContent());
+                    }
                 }
                 case "종료" -> {
                     return;
