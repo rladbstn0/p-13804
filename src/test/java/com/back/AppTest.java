8228b65c39d01647c11c8deda2f74987a46719f4
user.email <younsoo6103@naver.com>
2025-06-02 12:16:08 +0900

    step 6, b

--- src/test/java/com/back/AppTest.java
+++ src/test/java/com/back/AppTest.java
@@ -13,7 +13,9 @@ public class AppTest {
         String rs = AppTestRunner.run("");
 
         assertThat(rs)
-                .contains("== 명언 앱 ==");
+                .contains("== 명언 앱 ==")
+                .contains("명령) ");
+
     }
 
     @Test
@@ -27,7 +29,6 @@ public class AppTest {
                 """);
 
         assertThat(rs)
-                .contains("명령) ")
                 .contains("명언 : ")
                 .contains("작가 : ");
     }
