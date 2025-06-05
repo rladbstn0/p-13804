ec60ee3c302be323d1c07098d9f53aa16a516aee
user.email <younsoo6103@naver.com>
2025-06-05 10:15:01 +0900

    step 24, r

--- src/test/java/com/back/domain/wiseSaying/repository/WiseSayingFileRepositoryTest.java
+++ src/test/java/com/back/domain/wiseSaying/repository/WiseSayingFileRepositoryTest.java
@@ -33,4 +33,18 @@ public class WiseSayingFileRepositoryTest {
                 foundWiseSaying
         ).isEqualTo(wiseSaying);
     }
+
+    @Test
+    @DisplayName("2번째 등록에서는 2번 명언이 생성")
+    void t2() {
+        WiseSaying wiseSaying1 = new WiseSaying("꿈을 지녀라. 그러면 어려운 현실을 이길 수 있다.", "괴테");
+        wiseSayingFileRepository.save(wiseSaying1);
+
+        WiseSaying wiseSaying2 = new WiseSaying("나의 죽음을 적들에게 알리지 말라.", "이순신");
+        wiseSayingFileRepository.save(wiseSaying2);
+
+        wiseSayingFileRepository.save(wiseSaying2);
+
+        assertThat(wiseSaying2.getId()).isEqualTo(2);
+    }
 }
