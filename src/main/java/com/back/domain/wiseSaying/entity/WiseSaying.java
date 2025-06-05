f2cb389c0a1c3d279c7c789fb9f6daf08649abcf
user.email <younsoo6103@naver.com>
2025-06-05 09:56:25 +0900

    step 22, g

--- src/main/java/com/back/domain/wiseSaying/entity/WiseSaying.java
+++ src/main/java/com/back/domain/wiseSaying/entity/WiseSaying.java
@@ -5,6 +5,10 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Objects;
+
 @Getter
 @Setter
 @ToString
@@ -18,7 +22,34 @@ public class WiseSaying {
         this.author = author;
     }
 
+    public WiseSaying(Map<String, Object> map) {
+        this.id = (int) map.get("id");
+        this.content = (String) map.get("content");
+        this.author = (String) map.get("author");
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        WiseSaying that = (WiseSaying) o;
+        return id == that.id && Objects.equals(content, that.content) && Objects.equals(author, that.author);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, content, author);
+    }
+
     public boolean isNew() {
         return id == 0;
+    }
+
+    public Map<String, Object> toMap() {
+        Map<String, Object> map = new LinkedHashMap<>();
+
+        map.put("id", id);
+        map.put("content", content);
+        map.put("author", author);
+
+        return map;
     }
 }
--- src/main/java/com/back/domain/wiseSaying/repository/WiseSayingFileRepository.java
+++ src/main/java/com/back/domain/wiseSaying/repository/WiseSayingFileRepository.java
@@ -1,15 +1,30 @@
 package com.back.domain.wiseSaying.repository;
 
 import com.back.domain.wiseSaying.entity.WiseSaying;
+import com.back.standard.util.Util;
+
+import java.util.Map;
 
 public class WiseSayingFileRepository {
 
 
     public void save(WiseSaying wiseSaying) {
+        if (wiseSaying.isNew()) {
+            wiseSaying.setId(1);
+        }
 
+        Map<String, Object> wiseSayingMap = wiseSaying.toMap();
+        String wiseSayingJsonStr = Util.json.toString(wiseSayingMap);
+        Util.file.set("db/wiseSaying/1.json", wiseSayingJsonStr);
     }
 
     public WiseSaying findById(int id) {
-        return null;
+        String wiseSayingJsonStr = Util.file.get("db/wiseSaying/%d.json".formatted(id), "");
+
+        Map<String, Object> wiseSayingMap = Util.json.toMap(wiseSayingJsonStr);
+
+        return new WiseSaying(wiseSayingMap);
     }
+
+
 }
